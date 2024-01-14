27640d0930433ec2f8e2e2550d88feb9ad3f67b4
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-14 19:07:38 +0530

    Add files via upload

---
+++ WordCount.java
@@ -0,0 +1,10 @@
+package com.logic;
+
+import java.util.Scanner;
+
+public class WordCount {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println((sc.nextLine().split(" ")).length);
+}
+}
