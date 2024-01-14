903f57a6c0a014c63de1095b4432150452ea5dd8
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-14 22:53:26 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+package com.logic;
+
+import java.util.Scanner;
+
+public class ReverseString {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println(new StringBuffer(sc.nextLine()).reverse());
+}
+}
