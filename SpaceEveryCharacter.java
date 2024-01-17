796562cd2e23cd2777e64b8f2fa66a3e934bc8d6
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-01-17 08:59:31 +0530

    Add files via upload

---
+++ SpaceEveryCharacter.java
@@ -0,0 +1,13 @@
+package logic;
+
+import java.util.Scanner;
+
+public class SpaceEveryCharacter {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String s=sc.nextLine();
+	for(int i=0;i<s.length();i++) {
+		System.out.println(s.charAt(i)+" ");
+	}
+}
+}
