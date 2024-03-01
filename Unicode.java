7a69661fb7c7bf1737c2922579b3860672a00f8e
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-03-01 15:24:05 +0530

    Add files via upload

---
+++ Unicode.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class Unicode {
+public static void main(String[] args) {
+	int[] uni= {'\u0041','\u0042','\u0043','\u0044'};
+	for(int i=0;i<uni.length;i++)
+		System.out.println(uni[i]);
+}
+}
